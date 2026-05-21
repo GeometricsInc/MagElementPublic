82e3632c19f03b7da49de6884561ad700d716ab6
GeoSJ <41709521+GeoSJ@users.noreply.github.com>
2026-05-20 18:48:19 -0700

    Fix license typo and add error directive
    Corrected a typo in the license conditions and added an error directive for README information.

--- test/src/TestClient.cpp
+++ test/src/TestClient.cpp
@@ -6,7 +6,7 @@ copy of this software and associated documentation files (the “Software”),
 to deal in the Software without restriction, including without limitation 
 the rights to use, copy, modify, merge, publish, distribute, sublicense, 
 and/or sell copies of the Software, and to permit persons to whom the 
-Software is furnished to do so, subject to the following conditions:
+Software is furnished to do so, subject to the following condsitions:
 
 The above copyright notice and this permission notice shall be included 
 in all copies or substantial portions of the Software.
@@ -22,7 +22,7 @@ IN THE SOFTWARE.
 
 /* See LICENSE file for additional licenses */
 #define EXAMPLE_VERSION "1.2.6"
-
+#error Please see the README.md for information about the status of this software.
 
 #include <cstdlib>
 #include <cstring>
